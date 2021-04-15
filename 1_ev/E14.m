c3c09f7b85c125b30d83f3859b0829f6a12d621c
berylramadhian <berylramadhian@gmail.com>
2021-04-15 12:53:55 +0200

    fixed typo

--- 1_ev/E14.m
+++ 1_ev/E14.m
@@ -1,13 +1,13 @@
-A = [-4 10 8; 10 -7 2; 8 -2 3];
+A = [-4 10 8; 10 -7 -2; 8 -2 3];
 [V,D] = eig(A)
 n = length(A);
 I = eye(n);
 eigvals = diag(D);
-len = length(eigvals)
+len = length(eigvals);
 for i=1:len
   disp([">>>Shift no.: ", num2str(i)])
   sigma = diag(D)(i) #initial shift using the value from eig(A)
-  x = rand(n,1) #random pick
+  x = rand(n,1); #random pick
   [x, sigma] = inverse_iteration(A, I, x, sigma, 5)
   disp("================")
 end
