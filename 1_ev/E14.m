c4823fdcc3c4c0f8513413e3edec79dd61ba17b0
Beryl Aribowo <baribowo@sato.mat.univie.ac.at>
2021-04-16 16:58:33 +0200

    revised E14 in accordance to class discussion

--- 1_ev/E14.m
+++ 1_ev/E14.m
@@ -1,13 +1,14 @@
+format long;
+format compact;
 A = [-4 10 8; 10 -7 -2; 8 -2 3];
-[V,D] = eig(A)
+[V, D] = eig(A)
 n = length(A);
 I = eye(n);
 eigvals = diag(D);
-len = length(eigvals);
-for i=1:len
+for i=1:n
   disp([">>>Shift no.: ", num2str(i)])
-  sigma = diag(D)(i) #initial shift using the value from eig(A)
+  sigma = eigvals(i) #initial shift using the value from eig(A)
   x = rand(n,1); #random pick
-  [x, sigma] = inverse_iteration(A, I, x, sigma, 5)
+  [x, lambda] = inverse_iteration(A, I, x, sigma, 5)
   disp("================")
 end
--- 1_ev/inverse_iteration.m
+++ 1_ev/inverse_iteration.m
@@ -1,14 +1,15 @@
-function [x, sigma] = inverse_iteration(A, B, x, sigma, l)
-  for i=1:l
-    xl = inv(A - sigma*B)*(B*x);
+function [x, lambda] = inverse_iteration(A, B, x, sigma, el)
+  for i=1:el
+    xl = (A - sigma*B)\(B*x);
+    if ~all(isfinite(xl))
+      break;
+    end
     x = xl/norm(xl); #eigvec
-    sigma = (x'*A*x)/(x'*B*x); #rayleigh quotient for eigval
+    lambda = (x'*A*x)/(x'*B*x); #rayleigh quotient for eigval
     disp(["iter = ",num2str(i), ":"]);
     disp("eigvec = ");
     disp(x)
-    disp(["eigval = ", num2str(sigma)]);
+    disp(["eigval = ", num2str(lambda)]);
     disp("\n");
-%    disp(["l = ", num2str(l), ", eigenvector = ", num2str(x)]);
-%    disp(["l = ", num2str(l), ", eigenvalue = ",num2str(sigma)])
   end
 endfunction
