f379c28ef6c2bbee5854eff0830a1c38c8b03c28
berylramadhian <berylramadhian@gmail.com>
2021-05-20 18:54:10 +0200

    rev E30, added E40 and E41

--- 2_ode/E30.m
+++ 2_ode/E30.m
@@ -4,7 +4,7 @@ N = 1.5./h
 
 for it=1:length(h)
   disp(["h = ",num2str(h(it))]);
-  ym = zeros(N(it)+1, 4); #y(:,1)=exact, y(:,2)=euler, y(:,3)=first half, y(:,4)=second half, y(:,5)=runge kutta 
+  ym = zeros(N(it)+1, 5); #y(:,1)=exact, y(:,2)=euler, y(:,3)=first half, y(:,4)=second half, y(:,5)=runge kutta 
   ym(1,:) = 1;
   t = linspace(0, 1.5, N(it)+1);
   #exact:
@@ -27,12 +27,19 @@ for it=1:length(h)
     ym(i+1,4) = ym(i,4)+0.5*h(it)*(fy+fyk);
     
     #runge kutta:
-    
+    f1 = ym(i,5)-(2*t(i)/ym(i,5));
+    y2 = ym(i,5) + 0.5*h(it)*f1;
+    f2 = y2 - (2*t(i)/y2);
+    y3 = ym(i,5) + 0.5*h(it)*f2;
+    f3 = y3 - (2*t(i)/y3);
+    y4 = ym(i,5) + h(it)*f3;
+    f4 = y4 - (2*t(i)/y4);
+    ym(i+1,5) = ym(i,5) + h(it)*(f1 + (2*f2) + (2*f3) + f4)/6;
     
   endfor
 
-  plot(t,ym(:,1),"r", t,ym(:,2),"g", t,ym(:,3),"b", t,ym(:,4),"c");
-  title(["h = ",num2str(h(it))]); xlabel('t'); ylabel('y'); legend({'y exact','y euler','y 1halfstep', 'y 2halfstep'},'Location','northeast');
+  plot(t,ym(:,1),"r", t,ym(:,2),"g:", t,ym(:,3),"b--", t,ym(:,4),"c-o", t,ym(:,5),"y++");
+  title(["h = ",num2str(h(it))]); xlabel('t'); ylabel('y'); legend({'y exact','y euler','y 1halfstep', 'y 2halfstep', 'y RK'},'Location','northeast');
   figure()
     
 endfor
