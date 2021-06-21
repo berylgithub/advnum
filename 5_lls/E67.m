6b002375128e7a8eeee894edac1a7d2f949ece84
berylramadhian <berylramadhian@gmail.com>
2021-06-21 21:29:02 +0200

    E67 chkpt, adsorption and functional form left

--- 5_lls/E67.m
+++ 5_lls/E67.m
@@ -1,4 +1,4 @@
-% E67, Compute the minimum Cv
+% E67, Compute the minimum Cv and fills from an elimination ordering on a row*col grid
 rows = 4;
 cols = 3;
 M = grid_generator(rows, cols);
@@ -10,7 +10,7 @@ e = [1:rows*cols] % generate the elimination ordering
  % get the entries of M(i,j) = s, s = 1
 fill_count = 0;
 Cv_list = {};
-separator_idx_list = ones(len); % index of "|" in (J|K), used during absorption (generating supernodes)
+separator_idx_list = ones(rows*cols); % index of "|" in (J|K), used during absorption (generating supernodes)
 for Jv=e
   % Find Kv := v's neighbours:
   [i,j,s] = find(M);
@@ -25,15 +25,11 @@ for Jv=e
   % Calculate the fills:
   Kv_vec = cell2mat(Kv)(2:end);
   fill_idxes = nchoosek(Kv_vec, 2); % generate the fills' combination
-  disp(Jv)
-  disp(length(fill_idxes))
-  if length(fill_idxes) > 1
+  if length(fill_idxes) > 1 % if the combination is not empty
     for k=1:length(fill_idxes(:,1))
-  ##    disp([k, fill_idxes(k,1), fill_idxes(k,2)])
       if M(fill_idxes(k,1), fill_idxes(k,2)) == 0 % if no edge
-  ##      disp(["no edge = ",num2str(fill_idxes(k,1)), num2str(fill_idxes(k,2))]);
         M(fill_idxes(k,1), fill_idxes(k,2)) = M(fill_idxes(k,2), fill_idxes(k,1)) = 1; % add edge
-        fill_count += 1;
+        fill_count += 1; % add fill
       endif
     endfor
   endif
