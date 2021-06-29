dbfc69080de99bb561efae09e56e43f94fc13b57
berylramadhian <berylramadhian@gmail.com>
2021-06-29 12:21:54 +0200

    revised E61 - added LU factorization

--- 5_lls/E61.m
+++ 5_lls/E61.m
@@ -7,57 +7,79 @@ filename = "mm/jgl009.mtx" % small example
 disp(A);
 
 % E61(ii):
-filenames = ["mm/bcspwr01.mtx"; "mm/can___24.mtx"; "mm/fidapm05.mtx";
-            "mm/tols90.mtx"; "mm/qc324.mtx"; "mm/memplus.mtx"; "mm/saylr3.mtx"];
+filenames = ["mm/bcspwr01.mtx"; "mm/can___24.mtx"; "mm/fidapm05.mtx"; 
+"mm/tols90.mtx"; "mm/qc324.mtx"; "mm/memplus.mtx"; "mm/saylr3.mtx"];
+%
+plt_r = 3;
+plt_c = 4;
 for i=1:length(filenames)
   [rows, cols, entries, rep, field, symm] = mminfo(filenames(i,:));
   A = mmread(filenames(i,:));
-  nz(1) = nnz(A);
-  subplot(2,3,1);
+  [L,U,P,Q] = lu(A);
+  nz(1) = nnz(L);
+  subplot(plt_r,plt_c,1);
   spy(A);
   title(filenames(i,:));
+  subplot(plt_r,plt_c,5); #factor
+  spy(L);
   xlabel(["Nonzeros = ",num2str(nz(1))]);
-
+  title(['L (LU) - factor of ', filenames(i,:)])
+  
   % Column-Permutation:
   tic;
   r = colperm(A);
-  nz(2) = nnz(A(r,r));
   runtime(1) = toc;
-  subplot(2,3,2);
+  [L,U,P,Q] = lu(A(r,r));
+  nz(2) = nnz(L);
+  subplot(plt_r,plt_c,2);
   spy(A(r,r));
   title(['Column-Permutation of ', filenames(i,:)])
-  xlabel(["Nonzeros = ",num2str(nz(2)), "; runtime = ",num2str(runtime(1)),"s"]);
-
+  xlabel(["runtime = ",num2str(runtime(1)),"s"]);
+  subplot(plt_r,plt_c,6); #factor
+  spy(L);
+  xlabel(["Nonzeros = ",num2str(nz(2))]);
+  title(['L of Colperm of ', filenames(i,:)])
+  
   % Minimum-Degree:
   tic;
   r = amd(A);
   runtime(2) = toc;
-  nz(3) = nnz(A(r,r));
-  subplot(2,3,3);
+  [L,U,P,Q] = lu(A(r,r));
+  nz(3) = nnz(L);
+  subplot(plt_r,plt_c,3);
   spy(A(r,r));
   title(['Minimum-Degree of ', filenames(i,:)])
-  xlabel(["Nonzeros = ",num2str(nz(3)), "; runtime = ",num2str(runtime(2)),"s"]);
-
+  xlabel(["runtime = ",num2str(runtime(2)),"s"]);
+  subplot(plt_r,plt_c,7); #factor
+  spy(L);
+  xlabel(["Nonzeros = ",num2str(nz(3))]);
+  title(['L of MinDegree of ', filenames(i,:)])
+  
   % Cuthill-McKee:
   tic;
   r = symrcm(A);
   runtime(3) = toc;
-  nz(4) = nnz(A(r,r));
-  subplot(2,3,4);
+  [L,U,P,Q] = lu(A(r,r));
+  nz(4) = nnz(L);
+  subplot(plt_r,plt_c,4);
   spy(A(r,r));
   title(['Cuthill-McKee of ', filenames(i,:)])
-  xlabel(["Nonzeros = ",num2str(nz(4)), "; runtime = ",num2str(runtime(3)),"s"]);
-
+  xlabel(["runtime = ",num2str(runtime(3)),"s"]);
+  subplot(plt_r,plt_c,8); #factor
+  spy(L);
+  xlabel(["Nonzeros = ",num2str(nz(4))]);
+  title(['L of CM of ', filenames(i,:)])
+  
   %plot runtime
-  subplot(2,3,5);
+  subplot(plt_r,plt_c,9);
   labels = {'Column-Permutation', 'Min Degree', 'Cuthill-McKee'};
   bar(runtime);
   title(filenames(i,:));
   ylabel('Runtime');
   set(gca,'xticklabel',labels)
-  
+
   %plot nz
-  subplot(2,3,6);
+  subplot(plt_r,plt_c,10);
   labels = {'Default', 'Column-Permutation', 'Min Degree', 'Cuthill-McKee'};
   bar(nz);
   title(filenames(i,:));
