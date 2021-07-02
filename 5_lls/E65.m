f577d689f28ce24b724ddef0de643d436a1ea441
Beryl Aribowo <baribowo@sato.mat.univie.ac.at>
2021-07-02 18:11:37 +0200

    E65 checkpoint

---
+++ 5_lls/E65.m
@@ -0,0 +1,6 @@
+rows = 5
+cols = 3
+M = grid_generator(rows, cols);
+e = amd(M)
+spy(M(e,e))
+
---
+++ 5_lls/dijkstra_num.m
@@ -0,0 +1,3 @@
+function list_dist = dijkstra_num(G, source)
+  %dijkstra algo using sparse symmetric matrix as input
+endfunction
